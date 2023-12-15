d033282e4785150fc35fdcfbd577b907e16d48bf
Sohel1101 <sohelshaikhdiitkwd@gmail.com>
2023-12-15 20:47:11 +0530

    App.jsx create

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar/Navbar"
+import Header from "./Header/Header"
+import About from "./About/About"
+import Skills from "./Skills/Skills"
+import Footer from "./Footer/Footer"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Header></Header>
+      <About></About>
+      <Skills></Skills>
+      <Footer></Footer>
+        
+    </div>
+  )
+}
+
+export default App
