3ac91052a5c11aca7b6e1ea45441b64e531f5a2f
Sohel1101 <sohelshaikhdiitkwd@gmail.com>
2023-12-15 20:51:40 +0530

    Add Skills

---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,24 @@
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <div className='Skills'>
+        <h1>Skills</h1>
+        <div id='sk'>
+            <div className="skitems">HTML</div>
+            <div className="skitems">CSS</div>
+            <div className="skitems">JAVASCRIPT</div>
+            <div className="skitems">TAILWIND</div>
+            <div className="skitems">BOOTSTRAP</div>
+            <div className="skitems">GITHUB</div>
+            <div className="skitems">REACT</div>
+            <div className="skitems">JAVA</div>
+            <div className="skitems">SQL</div>
+            <div className="skitems">NETLIFY</div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Skills
