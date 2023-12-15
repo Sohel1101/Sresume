6cba3bcb8bfc40b3e6fba4483310ca16900d0fb1
Sohel1101 <sohelshaikhdiitkwd@gmail.com>
2023-12-15 20:48:54 +0530

    Add About

---
+++ src/About/About.jsx
@@ -0,0 +1,13 @@
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='About'>
+      <div className="aboutitems" id='a1'>About Me</div>
+      <div className="aboutitems" id='a2'>Lorem ipsum dolor sque, ipstis. Aodi iure laudantium maiores quibusdam nihil sapiente ratione, quaerat itaque culpa odio est modi! Voluptatum consequatur voluptas rerum nam! Possimus ad id alias mollitia fuga eligendi iure tempora doloremque? Cupiditate fugiat reprehenderit asperiores debitis dolorem, quae placeat blanditiis, expedita ullam itaque quasi maiores numquam pariatur incidunt sunt quia earum laudantium accusamus odio illo sed quisquam, sapiente eligendi assumenda? Totam quis placeat consequuntur officiis minima eius explicabo esse ipsam, velit repudiandae unde quisquam suscipit aliquam voluptatibus provident aliquid autem. Suscipit vel magnam necessitatibus earum ratione quaerat cum laboriosam est eum aliquam incidunt, aliquid dolorum ab recusandae maxime voluptatibus corrupti tenetur quidem. Vero tenetur sequi quaerat aut distinctio ut ullam consectetur quod excepturi perferendis praesentium adipisci perspiciatis molestiae illum totam voluptate qui ea animi nemo, omnis aperiam, voluptatibus similique, debitis porro. Omnis autem fuga tenetur aperiam? Officia, et possimus? Odit earum libero repudiandae nihil asperiores. Distinctio nam quibusdam sunt amet ullam. Obcaecati tenetur provident doloremque sequi distinctio dolorem asperiores commodi ratione amet perspiciatis magnam expedita, maxime veniam aut odio exercitationem incidunt, sapiente repellendus tempore voluptate veritatis nobis est consectetur. Aliquam, consequuntur? Tenetur neque incidunt nesciunt et voluptatem in earum nemo similique minus commodi, quis sint cumque?</div>
+      <div className="aboutitems" id='a3'><button>Download Resume</button></div>
+    </div>
+  )
+}
+
+export default About
