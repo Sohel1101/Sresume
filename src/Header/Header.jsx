c88535d8fd62612b04b3b441c3ed1edc4fff19c6
Sohel1101 <sohelshaikhdiitkwd@gmail.com>
2023-12-15 20:48:29 +0530

    Add Header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <div id="txt">
+      <h1>Sohel Shaikh</h1>
+      <h2>Web Developer</h2>
+      <div><button>Contact me</button></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
